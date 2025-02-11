4eb6294e910a2dc6452c320f609c684f77cf1adf
sxw0508 <112997708+sxw0508@users.noreply.github.com>
2025-02-11 14:23:59 +0800

    Update kernel.py

--- inference/kernel.py
+++ inference/kernel.py
@@ -20,15 +20,7 @@ def act_quant_kernel(x_ptr, y_ptr, s_ptr, BLOCK_SIZE: tl.constexpr):
     Returns:
         None
     """
-    pid = tl.program_id(axis=0)
-    offs = pid * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
-    x = tl.load(x_ptr + offs).to(tl.float32)
-    s = tl.max(tl.abs(x)) / 448.
-    y = x / s
-    y = y.to(y_ptr.dtype.element_ty)
-    tl.store(y_ptr + offs, y)
-    tl.store(s_ptr + pid, s)
-
+    return x.to(torch.bfloat16) * s.to(torch.bfloat16)
 
 def act_quant(x: torch.Tensor, block_size: int = 128) -> Tuple[torch.Tensor, torch.Tensor]:
     """
